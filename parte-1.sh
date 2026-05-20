00f362f87f78cb66c5995dceae35eac30052a62f
riccardogouthier-all <riccardogouthier@gmail.com>
2026-05-20 12:35:07 +0200

    parte-1 creata

---
+++ parte-1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Creare le directory se non esistono
+mkdir -p ./in
+
+while true; do
+    # Costruisce il nome del file col timestamp
+    filename="file_$(date '+%Y%m%d_%H%M%S').txt"
+    filepath="./in/$filename"
+
+    # Scrive il timestamp nel file
+    echo "$(date '+%Y-%m-%d %H:%M:%S')" > "$filepath"
+
+    echo "Creato: $filepath"
+
+    # Attendi 10 secondi
+    sleep 10
+done
